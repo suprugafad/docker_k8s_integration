31285eeae35e00eb0a9239a8d0fd7858d22a63ff
Alexandrina <poidaalesya382@gmail.com>
2023-06-15 12:40:33 +0300

    feat: Add migrations

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,8 @@ const config: ConnectionOptions = {
   password: 'poshik',
   database: 'mediumclone',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
 };
 
 export default config;
