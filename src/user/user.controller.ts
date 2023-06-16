a1bba82640c32c5f809f58b6d6258c3b48293b19
Alexandrina <poidaalesya382@gmail.com>
2023-06-16 11:18:06 +0300

    feat: Add registration and login in user module

---
+++ src/user/user.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from '@app/user/user.service';
+import { CreateUserDto } from '@app/user/dto/createUser.dto';
+import { UserResponseInterface } from '@app/user/types/userResponse.interface';
+import { LoginUserDto } from '@app/user/dto/loginUser.dto';
+
+@Controller()
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+  @Post('users')
+  @UsePipes(new ValidationPipe())
+  async createUser(
+    @Body('user') createUserDto: CreateUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.createUser(createUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+
+  @Post('users/login')
+  @UsePipes(new ValidationPipe())
+  async authenticateUser(
+    @Body('user') loginUserDto: LoginUserDto,
+  ): Promise<UserResponseInterface> {
+    const user = await this.userService.loginUser(loginUserDto);
+    return this.userService.buildUserResponse(user);
+  }
+}
