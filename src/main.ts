cddc8adab9c1a56d6e733ca40d75fc368fe99e43
Alexandrina <poidaalesya382@gmail.com>
2023-07-04 14:44:43 +0300

    feat: add docker

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ if (!process.env.IS_TS_NODE) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: __dirname + '/env/back.env' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,11 +2,11 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'localhost',
-  port: 5432,
+  host: 'db',
+  port: parseInt(process.env.DATABASE_PORT),
   username: 'mediumclone',
-  password: 'poshik',
-  database: 'mediumclone',
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
   migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
